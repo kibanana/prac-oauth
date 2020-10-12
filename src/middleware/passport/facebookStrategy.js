8e1b691a1e2b13a11361c9b915210de78631c3b7
Kim Yewo <kyw017763@gmail.com>
2020-10-12 12:00:10 +0900

    facebook oauth DB 로직 추가

--- src/middleware/passport/facebookStrategy.js
+++ src/middleware/passport/facebookStrategy.js
@@ -1,4 +1,5 @@
 const FacebookStrategy = require('passport-facebook').Strategy
+const userDB = require('../../models/user')
 
 module.exports = new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
@@ -6,7 +7,22 @@ module.exports = new FacebookStrategy({
     callbackURL: 'http://localhost:3000/auth/facebook/callback',
     profileFields: ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone',
       'updated_time', 'verified', 'displayName']
-  }, function (accessToken, refreshToken, profile, done) {
-    console.log(profile)
+  }, async function (_accessToken, _refreshToken, profile, done) {
+    try {
+      const { id, provider, _json: profileJson } = profile
+      const { name: fullName, last_name: lastName, first_name: firstName } = profileJson
+
+      const params = { id, type: provider, fullName, firstName, lastName }
+  
+      if (await userDB.IsExists({ type: params.type, id: params.id })) await userDB.SignIn(params)
+      else await userDB.SignUp(params)
+
+      const user = await userDB.GetItem({ type: params.type, id: params.type })
+      
+      return done(null, user)
+    }
+    catch (err) {
+        return done(null, { error: err })
+    }
   }
 )
